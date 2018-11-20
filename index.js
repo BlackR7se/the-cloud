fae61adf2c5e5cc8add538f87ddc9a5ba22e9d6d
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-19 21:36:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("Este servidor.", {type: "WATCHING"});
+  bot.user.setGame("con Zeus");
 
 });
 
